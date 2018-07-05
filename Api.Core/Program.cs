57052c72c354782e46921641afd2567c7c178e59
FreedomK <FreedomK@DIND10003367>
2018-07-05 19:36:15 +0200

    Merge

--- Api.Core/Program.cs
+++ Api.Core/Program.cs
@@ -1,4 +1,7 @@
 ﻿using FirebaseNet.DataLogic;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 using static System.Console;
 
 namespace Api.Core
@@ -8,28 +11,29 @@ namespace Api.Core
         public static void Main()
         {
             //Instantiating with base URL
-            var baseUrl = "https://accessrequestsapi.firebaseio.com/Teams/Team-Awesome";
+            var baseUrl = "https://accessrequestsapi.firebaseio.com/";
             FirebaseDB firebaseDB = new FirebaseDB(baseUrl);
 
             // Referring to Node with name "Teams"
             FirebaseDB firebaseDBTeams = firebaseDB.Node("Users");
+            var user = new User();
+            user.Email = "Nehemiah.Mackenzie@mail.com";
+            //user.Email = "Freedom.Khanyile@mail.com";
+            user.Status = "Active";
+            user.Role = "Developer";
 
             #region data to send
-            var data = @"
-                        {  
-                        
-                               'U1': {
-                                'UserName': 'FreedomK', 
-                               	'CloneUser': 'FakeD', 
-                               	'Email': 'Freedom.Khanyile@mail.com', 
-                               	'FirstName': 'Freedom',
-                               	'Surname': 'Khanyile',
-                               	'System': 'Automotive'
-                                     } 
-                                            
-                              
-                        } 
-                    ";
+            //var data = @"
+            //            {                          
+            //                'U1': {                                
+            //             	'Email': 'Freedom.Khanyile@mail.com', 
+            //             	'Status': 'Active',
+            //             	'Role': 'Developer'                            
+            //                   }                                           
+            //            } 
+            //        ";
+
+            string data = JsonConvert.SerializeObject(user);
 
             #endregion
 
@@ -39,16 +43,28 @@ namespace Api.Core
             WriteLine(getResponse.Success);
             if (getResponse.Success)
             {
-                WriteLine(getResponse.JSONContent);                
+                if (getResponse.JSONContent != "null")
+                {
+                    WriteLine(getResponse.JSONContent);
+                    var userList = JsonConvert.DeserializeObject<UserJson[]>(getResponse.JSONContent);
+                    WriteLine("node number is: {0}"  );
+                }
+                else
+                {
+                    WriteLine("No Users Yet");
+                    var putResponse1 = firebaseDBTeams.NodePath("U2").Put(data);
+                    WriteLine(putResponse1.Success);
+                }
+                            
             }
             WriteLine();
 
-          
-            //Putt method :
-            WriteLine("PUT request");
-            var putResponse = firebaseDBTeams.Put(data);
-            WriteLine(putResponse.Success);
-            WriteLine();
+
+            //Put method :
+            //WriteLine("PUT request");
+            //var putResponse = firebaseDBTeams.NodePath("U1").Put(data);
+            //WriteLine(putResponse.Success);
+            //WriteLine();
 
             //Post Method
             //WriteLine("POST Request");
@@ -58,16 +74,32 @@ namespace Api.Core
 
 
             //Still needs work will do ASAP
-            WriteLine("PATCH Request");
-            var patchResponse = firebaseDBTeams
-                // Use of NodePath to refer path lnager than a single Node  
-                .NodePath("Team-Awesome/Users/U1")
-                .Patch("{\"FreedomK\":\"King.Works@mail.com\"}");
-            WriteLine(patchResponse.Success);
-            WriteLine();
+            //WriteLine("PATCH Request");
+            //var patchResponse = firebaseDBTeams
+            //    // Use of NodePath to refer path lnager than a single Node  
+            //    .NodePath("U1")
+            //    .Patch("{\"Email\":\"Freedom.Khanyile@mail.com\"}");
+            //WriteLine(patchResponse.Success);
+            //WriteLine();
 
             WriteLine(firebaseDBTeams.ToString());
             ReadLine();
         }
     }
 }
+public class User
+{
+    [JsonProperty("Email")]
+    public string Email { get; set; }
+    [JsonProperty("Status")]
+    public string Status { get; set; }
+    [JsonProperty("Role")]
+    public string Role { get; set; }
+}
+
+public class UserJson
+{
+    [JsonProperty("User")]
+    public User User { get; set; }
+}
+
